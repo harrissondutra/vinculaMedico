e9dc243c206ccf540a9a7fa5374c1cc66bf1d44a
harrissondutra <harrissondutra@gmail.com>
2022-08-02 09:53:57 -0300

    Alteração da versão do Java

---
+++ src/test/java/br/com/estudo/vinculamedico/controller/EstabelecimentoControllerTest.java
@@ -0,0 +1,14 @@
+package br.com.estudo.vinculamedico.controller;
+
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class EstabelecimentoControllerTest {
+
+
+
+}
